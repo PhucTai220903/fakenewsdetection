8a69a053ccb59babc55999269c9ccaf47fc912f6
PhucTai220903 <phuctai.blc@gmail.com>
2023-12-29 23:12:28 +0700

    The First

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+from flask_cors import CORS
+import os
+import webbrowser
+from threading import Timer
+
+# Đặt tên thư mục mới cho templates
+template_folder = 'Design'
+app = Flask(__name__, template_folder=template_folder)
+CORS(app)
+
+with open('temp/svm_model.pkl', 'rb') as f:
+    svm_model = pickle.load(f)
+with open('temp/vectorized.pkl', 'rb') as f:
+    vectorized = pickle.load(f)
+
+@app.route('/')
+def index():
+    # Sử dụng os.path.join để kết hợp đường dẫn đến template
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.form.get('text')
+    if text is not None:
+        text_transformed = vectorized.transform([text])
+
+        prediction = svm_model.predict(text_transformed)[0]
+
+        return jsonify({'prediction': int(prediction)})
+    else:
+        return jsonify({'error': 'Input text not provided.'})
+
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
+if __name__ == '__main__':
+    if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
+        Timer(1, open_browser).start()
+    app.run(host='0.0.0.0', debug=True)
---
+++ readme.txt
@@ -0,0 +1,9 @@
+1. Chắc chắn máy đã cài python 3.11 hoặc version thấp hơn
+
+2. Mở terminal tại project và chạy lệnh  dưới:
+pip install -r requirements.txt
+
+3.đúp chuột app.py để chạy
+
+
+
